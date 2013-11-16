589431e8675c400efc4c41393d1c266be03f3c62
robertmryan <robert.ryan@mindspring.com>
2013-11-16 13:03:20 -0500

    Initial Commit

---
+++ Auto Layout Convenience Methods/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Auto Layout Convenience Methods
+//
+//  Created by Robert Ryan on 11/16/13.
+//  Copyright (c) 2013 Robert Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Auto Layout Convenience MethodsTests/Auto_Layout_Convenience_MethodsTests.m
@@ -0,0 +1,34 @@
+//
+//  Auto_Layout_Convenience_MethodsTests.m
+//  Auto Layout Convenience MethodsTests
+//
+//  Created by Robert Ryan on 11/16/13.
+//  Copyright (c) 2013 Robert Ryan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Auto_Layout_Convenience_MethodsTests : XCTestCase
+
+@end
+
+@implementation Auto_Layout_Convenience_MethodsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
